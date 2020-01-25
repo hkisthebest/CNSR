974334dbccf7c7b92139df84fd87d1fdd5ebfae9
hkisthebest <hkisthebest@gmail.com>
2020-01-26 00:42:30 +0800

    Fix: Console logs the audio details.

--- src/check.js
+++ src/check.js
@@ -7,6 +7,8 @@ module.exports = {
 	info1 = info1.streams[0];
 	let info2 = await ffprobe(file2, { path: ffprobeStatic.path});
 	info2 = info2.streams[0];
+	console.log({file1_sampleRate: info1.sample_rate, file1_channels: info1.channels, file1_bits_per_raw_sample: info1.bits_per_raw_sample});
+	console.log({file2_sampleRate: info2.sample_rate, file2_channels: info2.channels, file2_bits_per_raw_sample: info2.bits_per_raw_sample});
 	if(info1.sample_rate === info2.sample_rate && info1.channels === info2.channels && info1.bits_per_raw_sample === info2.bits_per_raw_sample){
 	    return true
 	}else{
